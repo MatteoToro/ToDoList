405b4b304413660d3af4e963b5f24298f6db78b4
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-01-29 23:40:37 +0100

    Creazione classe Task

---
+++ Task.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo Torelli on 29/01/26.
+//
+
+#include "Task.h"
---
+++ Task.h
@@ -0,0 +1,16 @@
+//
+// Created by Matteo Torelli on 29/01/26.
+//
+
+#ifndef TASK_H
+#define TASK_H
+
+
+
+class Task {
+
+};
+
+
+
+#endif //TASK_H
