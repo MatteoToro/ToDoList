146cb2605f8f8d12d60bd426a537a4c664a6d2cb
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-01-30 00:31:52 +0100

    Correzione del costruttore e dichiarazione dei metodi necessari al funzionamento della classe

--- Task.h
+++ Task.h
@@ -17,7 +17,17 @@ private:
 public:
   //Dichiaro i costruttori, di cui uno di default per avere un Task vuoto
   Task() = default;
-  Task(int id, std::string descrizione, bool stato);
+  Task(int id, std::string &descrizione, bool stato = false);
+
+  //Dichiaro le funzioni che utilizzerò
+  int getId() const;
+  std::string getDescrizione() const;
+  bool isStato() const;
+
+  //Dichiaro funzioni che permettono di trasformare o un oggetto Task in una stringa o una stringa in un oggetto Task
+  std::string convertiInStringa() const; //Trasforma un oggetto Task in stringa
+  static Task ricostruisciOggetto(const std::string &line);//Prende una stringa, letta dal file, e ricostruisce un oggetto
+
 
 
 
