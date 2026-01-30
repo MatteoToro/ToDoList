cbd23fc1db0ad2999ff5bb41faae3ed8e9390f6c
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-01-31 00:40:23 +0100

    Aggiunte due funzioni per camabire lo stato di un task

--- Task.h
+++ Task.h
@@ -19,11 +19,15 @@ public:
   Task() = default;
   Task(int id, std::string &descrizione, bool stato = false);
 
+
   //Dichiaro le funzioni che utilizzerò
   int getId() const;
   std::string getDescrizione() const;
   bool isStato() const;
 
+  //Dichiaro funzioni che permettono di cambiare lo stato di un task in fatto/non fatto
+  void segnaFatto();
+  void segnaNoFatto();
   //Dichiaro funzioni che permettono di trasformare o un oggetto Task in una stringa o una stringa in un oggetto Task
   std::string convertiInStringa() const; //Trasforma un oggetto Task in stringa
   static Task ricostruisciOggetto(const std::string &line);//Prende una stringa, letta dal file, e ricostruisce un oggetto
