1fccbc00ffff80f05f3fd4a8a93790fb7dcefd3f
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-01-31 23:31:02 +0100

    Membri privati

--- ToDoList.h
+++ ToDoList.h
@@ -5,9 +5,14 @@
 #ifndef TODOLIST_H
 #define TODOLIST_H
 
-
+#include "Task.h"
+#include <iostream>
+#include <vector>
 
 class ToDoList {
+  private:
+    std::vector<Task> attività;
+    int nextId = 1;//id progressivo
 
 };
 
