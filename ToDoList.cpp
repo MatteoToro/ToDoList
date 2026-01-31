6ae9923615435b27f6091600e0b42082768e5790
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-01-31 23:09:57 +0100

    Creazione della classe ToDoList

---
+++ ToDoList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo Torelli on 31/01/26.
+//
+
+#include "ToDoList.h"
---
+++ ToDoList.h
@@ -0,0 +1,16 @@
+//
+// Created by Matteo Torelli on 31/01/26.
+//
+
+#ifndef TODOLIST_H
+#define TODOLIST_H
+
+
+
+class ToDoList {
+
+};
+
+
+
+#endif //TODOLIST_H
