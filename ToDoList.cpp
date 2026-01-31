17e92716d762cb22af5a424449688dd06d4c9b84
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-02-01 00:03:28 +0100

    Definizione della funzione rimuoviTask

--- ToDoList.cpp
+++ ToDoList.cpp
@@ -8,3 +8,12 @@ void ToDoList::aggiungiTask(const std::string &descrizione){
   attività.push_back(Task(nextId++, descrizione, false));
 }
 
+bool ToDoList::rimuoviTask(int id){
+  for(std::size_t i = 0; i < attività.size(); i++){
+    if(attività[i].getId() == id){
+      attività.erase(attività.begin() + i);
+      return true;
+    }
+  }
+  return false;
+}
