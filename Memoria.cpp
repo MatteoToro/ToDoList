f00ace1d85b2c144a758281196974dedfe6ec5c6
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-02-02 00:13:44 +0100

    Definizione della funzione salva

--- Memoria.cpp
+++ Memoria.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Memoria.h"
+
+#include "Memoria.h"
+#include "Task.h"
+#include <fstream>//libreria utile per lavorare con i file
+#include <iostream>
+
+bool Memoria::salva(const ToDoList &lista, const std::string &nomeFile){
+  std::ofstream ofs(nomeFile.c_str());
+  if(!ofs)
+    return false;
+  for(const auto &a : lista.getAll()){
+    ofs << a.convertiInStringa() << std::endl;
+  }
+  return true;
+}
+
