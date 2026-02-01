a03669bea307a67b12141525b5238cb67b23092e
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-02-02 00:58:26 +0100

    MOdifica main

--- main.cpp
+++ main.cpp
@@ -6,20 +6,41 @@ int main() {
     ToDoList todo;
     Memoria::carica(todo, "todo.txt");
 
-    std::cout << "Todo attuale:\n";
-    for (const auto &t : todo.getAll()) {
-        std::cout << t.getId() << (t.isStato() ? " [x] " : " [ ] ") << t.getDescrizione() << "\n";
+    // Proviamo a caricare dal file
+    if (!Memoria::carica(todo, "todo.txt")) {
+        std::cout << "Nessun file trovato, creo una nuova todo list.\n";
     }
 
-    // esempio di uso:
+    std::cout << "Todo attuale:\n";
+
+    if (todo.getAll().empty()) {
+        std::cout << "(lista vuota)\n";
+    } else {
+        for (const auto &t : todo.getAll()) {
+            std::cout << t.getId()
+                      << (t.isStato() ? " [x] " : " [ ] ")
+                      << t.getDescrizione()
+                      << "\n";
+        }
+    }
+
+    // Esempio di utilizzo
     todo.aggiungiTask("Comprare il latte");
     todo.aggiungiTask("Studiare C++");
-    todo.segnaFatto(1); // marca il primo come fatto
+    todo.aggiungiTask(1);
+
+    std::cout << "\nDopo le modifiche:\n";
+    for (const auto &t : todo.getAll()) {
+        std::cout << t.getId()
+                  << (t.isStato() ? " [x] " : " [ ] ")
+                  << t.getDescrizione()
+                  << "\n";
+    }
 
     if (!Memoria::salva(todo, "todo.txt")) {
         std::cerr << "Errore nel salvataggio\n";
     } else {
-        std::cout << "Salvato in todo.txt\n";
+        std::cout << "\nSalvato in todo.txt\n";
     }
 
     return 0;
