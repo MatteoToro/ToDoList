5237f4a8af860a232a402acca60f78a0cafd6ce6
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-02-02 23:34:05 +0100

    Correzioni

--- Test.cpp
+++ Test.cpp
@@ -3,20 +3,20 @@
 //
 
 #include "gtest/gtest.h"
-#include "../src/TodoList.h"
-#include "../src/Memoria.h"
+#include "TodoList.h"
+#include "Memoria.h"
 #include <cstdio>
 
 
 TEST(ToDoListTest, AggiungiTrova){
-ToDolist lista;
+ToDoList lista;
 
 lista.aggiungiTask("T1");
 lista.aggiungiTask("T2");
 EXPECT_EQ(lista.getAll().size(), 2u);
 
 Task *t = lista.trovaTask(1);
-ASSERT_NE(T,nullptr);
+ASSERT_NE(t,nullptr);
 EXPECT_EQ(t->getDescrizione(), "T1");
 }
 
@@ -27,9 +27,9 @@ TEST(ToDoListTest, SalvaCarica){
 
   EXPECT_TRUE(Memoria::salva(lista, "Test.txt"));
 
-  ToDoList loaded;
+  ToDoList caricato;
   EXPECT_TRUE(Memoria::carica(caricato, "Test.txt"));
-  EXPECT_EQ(loaded.getAll().size(), 1u);
+  EXPECT_EQ(caricato.getAll().size(), 1u);
 
   std::remove("Test.txt");
 
