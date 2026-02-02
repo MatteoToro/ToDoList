dd2644f642289b5cbb7bcd0c35bff3697dd9c43c
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-02-02 23:26:02 +0100

    Test per verificare il corretto caricamento e salvataggio su file

--- Test.cpp
+++ Test.cpp
@@ -8,7 +8,7 @@
 #include <cstdio>
 
 
-TEST(ToDoListTest, AddAndFind){
+TEST(ToDoListTest, AggiungiTrova){
 ToDolist lista;
 
 lista.aggiungiTask("T1");
@@ -18,4 +18,19 @@ EXPECT_EQ(lista.getAll().size(), 2u);
 Task *t = lista.trovaTask(1);
 ASSERT_NE(T,nullptr);
 EXPECT_EQ(t->getDescrizione(), "T1");
+}
+
+TEST(ToDoListTest, SalvaCarica){
+  ToDoList lista;
+  lista.aggiungiTask("Salva");
+  lista.segnaFatto(1);
+
+  EXPECT_TRUE(Memoria::salva(lista, "Test.txt"));
+
+  ToDoList loaded;
+  EXPECT_TRUE(Memoria::carica(caricato, "Test.txt"));
+  EXPECT_EQ(loaded.getAll().size(), 1u);
+
+  std::remove("Test.txt");
+
 }
