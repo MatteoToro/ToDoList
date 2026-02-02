92cd3e81a9867f59fc62bbbf51935dc017c67c32
MatteoTorelli <matteo.torelli1@edu.unifi.it>
2026-02-02 23:19:16 +0100

    Test per verificare che trovaTask trovi il task giusto

--- Test.cpp
+++ Test.cpp
@@ -14,4 +14,8 @@ ToDolist lista;
 lista.aggiungiTask("T1");
 lista.aggiungiTask("T2");
 EXPECT_EQ(lista.getAll().size(), 2u);
+
+Task *t = lista.trovaTask(1);
+ASSERT_NE(T,nullptr);
+EXPECT_EQ(t->getDescrizione(), "T1");
 }
